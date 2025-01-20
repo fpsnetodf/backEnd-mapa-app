c12a9e7479fefd188879c676e7c0217dd65e2cf0
fpsnetodf <fps_netodf@hotmail.com>
2025-01-20 18:10:28 -0300

    allow-oritin

--- core/settings.py
+++ core/settings.py
@@ -56,9 +56,16 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'core.urls'
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+    # "http://localhost:3001", 
+    # "http://127.0.0.1:3001", 
+    # "https://cc32-131-72-220-133.ngrok-free.app", 
+    # ]
+    
+
 
 
 TEMPLATES = [
